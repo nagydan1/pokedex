c5c6f503929a62e1ccf5a93a8efdf7fd73ac6362
Daniel Nagy <daniel.nagy.w@gmail.com>
2024-07-06 16:12:15 +0100

    color mode switch

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
+import { ColorModeSwitch } from "./ColorModeSwitch";
 import logo from "../assets/pokemon_logo_PNG12.png";
 
 const NavBar = () => {
   return (
-    <HStack p={2}>
+    <HStack p={2} justifyContent='space-between'>
       <Image src={logo} boxSize="60px" />
-      <Text>Nav Bar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 };
