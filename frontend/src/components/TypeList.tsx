c31fcd6e5a4654419951a581f87cbcb3e733a755
Daniel Nagy <daniel.nagy.w@gmail.com>
2024-07-30 17:57:46 +0100

    spinner while fetching types

--- frontend/src/components/TypeList.tsx
+++ frontend/src/components/TypeList.tsx
@@ -1,17 +1,22 @@
-import { List, ListItem, Text } from "@chakra-ui/react";
+import { List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useTypes from "../hooks/useTypes";
 
 const TypeList = () => {
-  const { resourceList } = useTypes();
+  const { resourceList, isLoading, error } = useTypes();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
 
   return (
     <List>
       {resourceList.map((type) => (
         <ListItem key={type.name} paddingY="5px">
-          <Text fontSize='lg'>{type.name.charAt(0).toUpperCase() + type.name.slice(1)}</Text>
+          <Text fontSize="lg">
+            {type.name.charAt(0).toUpperCase() + type.name.slice(1)}
+          </Text>
         </ListItem>
       ))}
-    </List> 
+    </List>
   );
 };
 
