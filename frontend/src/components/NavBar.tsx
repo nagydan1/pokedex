e3c4f1482a0309123f8c5ba96d1d68fae8a5ce98
Daniel Nagy <daniel.nagy.w@gmail.com>
2024-10-13 17:27:44 +0100

    Build search input

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,13 +1,17 @@
-import { Heading, HStack, Image } from "@chakra-ui/react";
+import { Heading, HStack, Image, Show } from "@chakra-ui/react";
 import { ColorModeSwitch } from "./ColorModeSwitch";
 import logo from "../assets/pokemon_logo_PNG12.png";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack p={3} justifyContent="space-between">
-      <HStack gap={6}>
+    <HStack p={3} gap={3}>
+      <HStack gap={6} width="100%">
         <Image src={logo} boxSize="60px" />
-        <Heading my={2}>Pokedex</Heading>
+        <Show above="md">
+          <Heading my={2}>Pokedex</Heading>
+        </Show>
+        <SearchInput />
       </HStack>
       <ColorModeSwitch />
     </HStack>
